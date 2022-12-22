06c955eed6d65dc4d45891f623c9d2445bf3ee6d
Kavindu-J22 <it21032806@my.sliit.lk>
2022-12-22 16:24:39 +0530

    update

--- Ayanna Kiyanna Management System/frontend/src/pages/LandingPage/LandingPage.jsx
+++ Ayanna Kiyanna Management System/frontend/src/pages/LandingPage/LandingPage.jsx
@@ -17,69 +17,57 @@ export default function Starting({ history }) {
 
   return (
     <div>
-    <div className="header">
-      <video
-        autoPlay
-        loop
-        muted
-        style={{
-          position: "absolute",
-          width: "100%",
-          left: "50%",
-          top: "50%",
-          height: "100%",
-          objectFit: "cover",
-          transform: "translate(-50%, -50%)",
-          zIndex: "0",
-        }}
-      >
-      <source src={backgroundTheme} type="video/mp4" />
-      </video>
+      <section>
+       <div className="header">
+         <video
+           autoPlay
+           loop
+           muted
+           style={{
+              position: "absolute",
+              width: "100%",
+              left: "50%",
+              top: "50%",
+              height: "100%",
+              objectFit: "cover",
+              transform: "translate(-50%, -50%)",
+              zIndex: "0",
+            }}>
+            <source src={backgroundTheme} type="video/mp4" />
+          </video>
 
-      <nav className="nav">
-        
-        <ul className="nav-links">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Services</a> 
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-          <li>
-           <a href="/login" className="signup">Log In</a>
-         </li>
-
-        </ul>
-      </nav>
-
-      <div className="content">
-        <img className="logo1" src= {AKlogo}  alt="" />
-        
-        <h1>Welcome to <span>Ayanna Kiyanna..!</span></h1>
-        <h2>Jagath Kumara Jayasinghe</h2><br />
-        <p>A motivated, adaptable, and responsible computing undergraduate
+          <div className="content">
+            <img className="logo1" src= {AKlogo}  alt="" />
+            <h1>Welcome to <span>Ayanna Kiyanna..!</span></h1>
+            <h2>Jagath Kumara Jayasinghe</h2><br />
+            <p>A motivated, adaptable, and responsible computing undergraduate
                         seeking a position in an IT position which will utilize the professional,
                         technical and skills developed through keep practicing in this field. I can provide clean code and pixel
-                        perfect design. I also make the website mere & more interactive with web animations.</p><br></br>
-        <form>
-          <Link to="/login">
-            <button id ="LGbtn" type="submit">Explore</button>
-          </Link>
-          <br /><br />
-          <Link to="/teacher-login">
-            <button id ="LGbtnA" type="submit">For Admin</button>
-          </Link>
-        </form>
-      </div>
-    </div>
+                        perfect design. I also make the website mere & more interactive with web animations.
+            </p><br/><br/>
 
-    <section class="about">
+            <form>
+              <Link to="/login">
+              <button id ="LGbtn" type="submit">Explore</button>
+              </Link>
+              <br /><br />
+              <Link to="/teacher-login">
+              <button id ="LGbtnA" type="submit">Register With Us</button>
+              </Link>
+            </form>
+          </div>
+        </div>
+      </section>
+
+
+
+
+
+
+
+
+
+      <section class="about">
             <div class="main">
                 <img class="image-About" src={Ayanna} alt=""/>
                 <div class="about-text">
