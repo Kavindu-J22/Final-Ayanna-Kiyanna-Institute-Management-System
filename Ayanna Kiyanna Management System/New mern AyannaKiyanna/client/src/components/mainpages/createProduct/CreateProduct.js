9f14bb94e76fc441202611c5554d206b8ce7482c
Kavindu-J22 <it21032806@my.sliit.lk>
2022-12-30 02:18:06 +0530

    upload

--- Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/createProduct/CreateProduct.js
+++ Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/createProduct/CreateProduct.js
@@ -8,8 +8,8 @@ const initialState = {
     product_id: '',
     title: '',
     price: '',
-    description: 'How to and tutorial videos of cool CSS effect, Web Design ideas,JavaScript libraries, Node.',
-    content: 'Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.',
+    description: '',
+    content: '',
     category: '',
     _id: ''
 }
@@ -138,39 +138,39 @@ function CreateProduct() {
 
             <form onSubmit={handleSubmit}>
                 <div className="row">
-                    <label htmlFor="product_id">Product ID</label>
+                    <label htmlFor="product_id">කාණ්ඩ අංකය (ID)</label>
                     <input type="text" name="product_id" id="product_id" required
                     value={product.product_id} onChange={handleChangeInput} disabled={onEdit} />
                 </div>
 
                 <div className="row">
-                    <label htmlFor="title">Title</label>
+                    <label htmlFor="title">මාතෘකාව</label>
                     <input type="text" name="title" id="title" required
                     value={product.title} onChange={handleChangeInput} />
                 </div>
 
                 <div className="row">
-                    <label htmlFor="price">Price</label>
-                    <input type="text" name="price" id="price" required
+                    <label htmlFor="price">කොටස/මිල</label>
+                    <input type="number" name="price" id="price" required
                     value={product.price} onChange={handleChangeInput} />
                 </div>
 
                 <div className="row">
-                    <label htmlFor="description">Description</label>
+                    <label htmlFor="description">හැඳින්වීම</label>
                     <textarea type="text" name="description" id="description" required
                     value={product.description} rows="5" onChange={handleChangeInput} />
                 </div>
 
                 <div className="row">
-                    <label htmlFor="content">Content</label>
+                    <label htmlFor="content">අන්තර්ගතය</label>
                     <textarea type="text" name="content" id="content" required
                     value={product.content} rows="7" onChange={handleChangeInput} />
                 </div>
 
                 <div className="row">
-                    <label htmlFor="categories">Categories: </label>
+                    <label htmlFor="categories">ප්‍රවර්ගය: </label>
                     <select name="category" value={product.category} onChange={handleChangeInput} >
-                        <option value="">Please select a category</option>
+                        <option value="">කරුණාකර ප්‍රවර්ගය ඇතුලත් කරන්න</option>
                         {
                             categories.map(category => (
                                 <option value={category._id} key={category._id}>
--- Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/products/Filters.js
+++ Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/products/Filters.js
@@ -18,9 +18,9 @@ function Filters() {
     return (
         <div className="filter_menu">
             <div className="row">
-                <span>Filters: </span>
+                <span>වෙන්කරන්න : </span>
                 <select name="category" value={category} onChange={handleCategory} >
-                    <option value=''>All Products</option>
+                    <option value=''>සියලුම විශේෂාංග</option>
                     {
                         categories.map(category => (
                             <option value={"category=" + category._id} key={category._id}>
@@ -31,16 +31,16 @@ function Filters() {
                 </select>
             </div>
 
-            <input type="text" value={search} placeholder="Enter your search!"
+            <input type="text" value={search} placeholder="ඔබගේ සෙවුම ඇතුලත් කරන්න!"
             onChange={e => setSearch(e.target.value.toLowerCase())} />
 
             <div className="row sort">
-                <span>Sort By: </span>
+                <span>පිළිවෙල සකසන්න : </span>
                 <select value={sort} onChange={e => setSort(e.target.value)} >
-                    <option value=''>Newest</option>
-                    <option value='sort=oldest'>Oldest</option>
-                    <option value='sort=-price'>Price: Hight-Low</option>
-                    <option value='sort=price'>Price: Low-Hight</option>
+                    <option value=''>නවතම</option>
+                    <option value='sort=oldest'>මුල සිට</option>
+                    <option value='sort=-price'>කොටස/මිල : වැඩි-අඩු</option>
+                    <option value='sort=price'>කොටස/මිල : අඩු-වැඩි</option>
                 </select>
             </div>
         </div>
