6ee97dc2ffd73ea2ad2ee89fde70998794d1b27b
Kavindu-J22 <it21032806@my.sliit.lk>
2022-12-31 22:02:58 +0530

    upload

--- Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/Other/OnlineExams/OEgrade.js
+++ Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/Other/OnlineExams/OEgrade.js
@@ -1,27 +1,28 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import "./oegrade.css"
 
 
 function Oegrade() {
 
     return (
-        <div className="v-main-box">
+        <div className="Oegrade-main-box">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-            <section className='v-section'>
-                <div className='v-container'>
+            <section className='Oegrade-section'>
+                <div className='Oegrade-container'>
                 <h1>✥ Online Exams </h1>
                 <h3>සිංහල භාෂාව හා සාහිත්‍යය</h3>
                     <h4>“අයන්න කියන්න” නමින් සිංහල සඟරාවක් පළ කිරීමේ අවශ්‍යතාව අප තුළ ඇතිව තිබූයේ මීට වසර ගණනාවකට ඔබ්බෙහි සිට ය. නොයෙකුත් වූ බාධකයන් හේතුවෙන් දිනෙන් දින ප්‍රමාද වූ එකී සංකල්පය ප්‍රයෝගික යථාර්ථයක් ලෙසින් එළිදැක්වීමට අවස්ථාව උදා වූයේ මෙම වසරේදී ය.</h4>
-                    <div className='v-cards'>
-                        <div className='v-text-card'>
-                        <Link className='v-link' to="/Oegrade/Olonline">
+                    <div className='Oegrade-cards'>
+                        <div className='Oegrade-text-card'>
+                        <Link className='Oegrade-link' to="/Oegrade/Olonline">
                             <i class="fa-solid fa-newspaper"></i>
                             <h3>O/L</h3>
                             <h5>“අයන්න කියන්න” නමින් සිංහල සඟරාවක් පළ කිරීමේ අවශ්‍යතාව අප තුළ ඇතිව තිබූයේ මීට වසර ගණනාවකට ඔබ්බෙහි සිට ය.</h5>   
                         </Link>
                         </div>
-                        <div className='v-text-card'>
-                        <Link className='v-link' to="/Oegrade/AlOnline">
+                        <div className='Oegrade-text-card'>
+                        <Link className='Oegrade-link' to="/Oegrade/AlOnline">
                             <i class="fa-solid fa-newspaper"></i>
                             <h3>A/L</h3>
                             <h5>“අයන්න කියන්න” නමින් සිංහල සඟරාවක් පළ කිරීමේ අවශ්‍යතාව අප තුළ ඇතිව තිබූයේ මීට වසර ගණනාවකට ඔබ්බෙහි සිට ය.</h5>
--- Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/Other/OnlineExams/OeOlExList.js
+++ Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/Other/OnlineExams/OeOlExList.js
@@ -1,49 +1,50 @@
 import React from 'react'
+import "./exList.css"
 
 
 function Olonline() {
 
     return (
-        <div className="v-main-box">
+        <div className="exList-main-box">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-            <section className='B-section'>
-                <div className='B-container'>
+            <section className='exList-section'>
+                <div className='exList-container'>
                     <h2>✥ සිංහල භාෂාව හා සාහිත්‍යය </h2>
                     <h3> O/L Online Exams List </h3>
-                    <div className='B-cards'>
-                        <div className='B-text-card'>
+                    <div className='exList-cards'>
+                        <div className='exList-text-card'>
                             <i class="fa-solid fa-newspaper"></i>
                             <h3>Exam 1</h3>
 
-                                <a href="#" class="B-button" type="submit">Attempt</a>
+                                <a href="#" class="exList-button" type="submit">Attempt</a>
                             
                         </div>
-                        <div className='B-text-card'>
+                        <div className='exList-text-card'>
                             <i class="fa-solid fa-newspaper"></i>
                             <h3>Exam 2</h3>
                             
-                                <a href="#" class="B-button" type="submit">Attempt</a>
+                                <a href="#" class="exList-button" type="submit">Attempt</a>
                             
                         </div>
-                        <div className='B-text-card'>
+                        <div className='exList-text-card'>
                             <i class="fa-solid fa-newspaper"></i>
                             <h3>Exam 3</h3>
                             
-                                <a href="#" class="B-button" type="submit">Attempt</a>
+                                <a href="#" class="exList-button" type="submit">Attempt</a>
                             
                         </div>
-                        <div className='B-text-card'>
+                        <div className='exList-text-card'>
                             <i class="fa-solid fa-newspaper"></i>
                             <h3>Exam 4</h3>
                             
-                                <a href="#" class="B-button" type="submit">Attempt</a>
+                                <a href="#" class="exList-button" type="submit">Attempt</a>
                             
                         </div>
-                        <div className='B-text-card'>
+                        <div className='exList-text-card'>
                             <i class="fa-solid fa-newspaper"></i>
                             <h3>Exam 5</h3>
                             
-                                <a href="#" class="B-button" type="submit">Attempt</a>
+                                <a href="#" class="exList-button" type="submit">Attempt</a>
                             
                         </div>
                     </div>
