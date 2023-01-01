f3b9019aa827fc61b870ab3e27e358e194d62ba4
Kavindu-J22 <it21032806@my.sliit.lk>
2023-01-01 07:48:05 +0530

    update

--- Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/headers/NewHeader.js
+++ Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/headers/NewHeader.js
@@ -3,6 +3,7 @@ import {GlobalState} from '../../GlobalState'
 import {Link} from 'react-router-dom'
 import axios from 'axios'
 import "./newHeader.css"
+import Aklogo from "../assets/images/AKlogo.png"
 
 function NewHeader() {
     const state = useContext(GlobalState)
@@ -43,11 +44,14 @@ function NewHeader() {
     return (
         <section className='newheader'>
 
+            <img className="logoHead" src= {Aklogo}  alt="" />
+
+
             <ul style={styleMenu}>
-                <li><Link className='newheadlink' to="#" data-text="Home">{isAdmin ? 'Home' : 'Home'}</Link></li>
-                <li><Link className='newheadlink' to="#" data-text="Info">{isAdmin ? 'Info' : 'Info'}</Link></li>
-                <li><Link className='newheadlink' to="#" data-text="About">{isAdmin ? 'About' : 'About'}</Link></li>
-                <li><Link className='newheadlink' to="#" data-text="Services">{isAdmin ? 'Services' : 'Services'}</Link></li>
+                <li><Link className='newheadlink' to="/" data-text="Home">{isAdmin ? 'Home' : 'Home'}</Link></li>
+                <li><Link className='newheadlink' to="/Home/Info" data-text="Info">{isAdmin ? 'Info' : 'Info'}</Link></li>
+                <li><Link className='newheadlink' to="/Home/About" data-text="About">{isAdmin ? 'About' : 'About'}</Link></li>
+                <li><Link className='newheadlink' to="/Home/Service" data-text="Services">{isAdmin ? 'Services' : 'Services'}</Link></li>
                 <li><Link className='newheadlink' to="#" data-text="Contact">{isAdmin ? 'Contact' : 'Contact'}</Link></li>
 
                 {isAdmin && adminRouter()}
