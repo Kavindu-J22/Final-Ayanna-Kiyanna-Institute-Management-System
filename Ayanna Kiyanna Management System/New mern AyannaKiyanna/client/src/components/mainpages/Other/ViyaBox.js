f1e412c37d4017fbe68f55da929ca12bab827a98
Kavindu-J22 <it21032806@my.sliit.lk>
2023-01-02 00:54:27 +0530

    update

--- Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/Other/ViyaBox.js
+++ Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/Other/ViyaBox.js
@@ -154,6 +154,23 @@ function ViyaBox() {
                     </div>
                 
             </section>
+            <section className='v-section'>
+                
+                    <div className='v-cards'>
+
+                        <div className='v-text-card' id='v-text-card-lastitem'>
+                            <div className='v-content'>
+                                <h2>12</h2>
+                                
+                                <h3>ශ්‍රේණියේ අනුව හදාරන්න</h3>
+                                <h5>“අයන්න කියන්න” නමින් සිංහල සඟරාවක් පළ කිරීමේ අවශ්‍යතාව අප තුළ ඇතිව තිබූයේ මීට වසර ගණනාවකට ඔබ්බෙහි සිට ය.</h5>   
+                                <a href='#'>Explore</a>
+                                </div>
+                        </div>
+
+                    </div>
+                
+            </section>
 
             </div>
     )
