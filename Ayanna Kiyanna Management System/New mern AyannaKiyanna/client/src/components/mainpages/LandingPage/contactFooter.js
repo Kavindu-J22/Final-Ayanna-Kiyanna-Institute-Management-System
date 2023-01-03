834537025b035b8764b05942ae89bcd5990a747c
Kavindu-J22 <it21032806@my.sliit.lk>
2023-01-04 03:08:01 +0530

    upload

--- Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/LandingPage/contactFooter.js
+++ Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/LandingPage/contactFooter.js
@@ -6,10 +6,34 @@ function Contactfooter(){
 
   return(
 
-    <div>
-        <section className="Contact-Footer-main">
+    <div className="my-contact-big">
+
+        <div className="Contact-Footer-main">
+            <footer className="myfooter">
+                <div className="Fwaves">
+                    <div className="wave" id="wave1"></div>
+                    <div className="wave" id="wave2"></div>
+                    <div className="wave" id="wave3"></div>
+                    <div className="wave" id="wave4"></div>
+                </div>
+                <ul className="myfooter-social-icons">
+                    <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-facebook-messenger"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-whatsapp"></i></a></li>
+                </ul>
+                <ul className="myFooter-menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Info</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">For-You</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <p>©Sinhala Institute Of Ayanna Kiyanna | All Rights Reserved</p>
+            </footer>
         
-        </section> 
+        </div> 
     </div>
   );
 }
