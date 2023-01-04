246f3bcdafd614dcc849e81e8f68f8f884c831c4
Kavindu-J22 <it21032806@my.sliit.lk>
2023-01-04 18:05:45 +0530

    update

--- Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/LandingPage/contactFooter.js
+++ Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/LandingPage/contactFooter.js
@@ -3,7 +3,7 @@ import "./contactfooter.css";
 
 
 function Contactfooter(){
-    var i;
+    
   return(
    
         <div className="Contact-Footer-main">
@@ -12,12 +12,12 @@ function Contactfooter(){
 
             <div className="SosialIconRack">
                 <ul>
-                    <li style={i} id="SR1"><a href="#"><span><i class="fa-brands fa-facebook"></i></span> facebook</a></li>
-                    <li style={i} id="SR2"><a href="#"><span><i class="fa-brands fa-whatsapp"></i></span> whatsapp</a></li>
-                    <li style={i} id="SR3"><a href="#"><span><i class="fa-brands fa-facebook-messenger"></i></span> Massenger</a></li>
-                    <li style={i} id="SR4"><a href="#"><span><i class="fa-brands fa-instagram"></i></span> instagram</a></li>
-                    <li style={i} id="SR5"><a href="#"><span><i class="fa-brands fa-youtube"></i></span> Youtube</a></li>
-                    <li style={i} id="SR6"><a href="#"><span><i class="fa-solid fa-envelope"></i></span> G-Mail</a></li>
+                    <li id="SR1"><a href="#"><span><i class="fa-brands fa-facebook"></i></span> facebook</a></li>
+                    <li id="SR2"><a href="#"><span><i class="fa-brands fa-whatsapp"></i></span> whatsapp</a></li>
+                    <li id="SR3"><a href="#"><span><i class="fa-brands fa-facebook-messenger"></i></span> Massenger</a></li>
+                    <li id="SR4"><a href="#"><span><i class="fa-brands fa-instagram"></i></span> instagram</a></li>
+                    <li id="SR5"><a href="#"><span><i class="fa-brands fa-youtube"></i></span> Youtube</a></li>
+                    <li id="SR6"><a href="#"><span><i class="fa-solid fa-envelope"></i></span> G-Mail</a></li>
                 </ul>
             </div>
         </div>
