9e8fdf5a0e7ccfd9d0f287105316d6bd0a175249
Kavindu-J22 <it21032806@my.sliit.lk>
2023-01-11 03:02:08 +0530

    update

--- Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/App.js
+++ Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/App.js
@@ -8,31 +8,31 @@ import { Link } from 'react-router-dom';
 
 
 function App() {
-  const [topshow, settopShow] = useState(false)
+//   const [topshow, settopShow] = useState(false)
 
-  const contralbackbtn = ()=>{
-    if(window.scrollY>=120) {
-      settopShow(true)
-    }else{
-      settopShow(false)
-    }
-}
-useEffect(() => {
-    window.addEventListener('scroll',
-    contralbackbtn)
-    return () => {
-        window.removeEventListener('scroll',
-        contralbackbtn)
-    }
-},[])
+//   const contralbackbtn = ()=>{
+//     if(window.scrollY>=120) {
+//       settopShow(true)
+//     }else{
+//       settopShow(false)
+//     }
+// }
+// useEffect(() => {
+//     window.addEventListener('scroll',
+//     contralbackbtn)
+//     return () => {
+//         window.removeEventListener('scroll',
+//         contralbackbtn)
+//     }
+// },[])
 
   return (
     <DataProvider>
       <Router>
         <div className="App">
           <MainPages />
-          <div className='newthinkMenu'><Link  to="/Header/NewHeader"><i class="fa-solid fa-bars"></i></Link></div>
-          <div className={`toptoptop ${topshow && 'toptoptop-styles'}`}><a className='gotopbtn' href='#'><i class="fa-solid fa-up-long"></i></a></div>
+          <div className='newthinkMenu'><a href="/Header/NewHeader"><i class="fa-solid fa-bars"></i></a></div>
+          {/* <div className={`toptoptop ${topshow && 'toptoptop-styles'}`}><Link to='/'><i class="fa-solid fa-up-long"></i></Link></div> */}
         </div>
 
       </Router>
