66521d8269c54e894499f5fac7cfd83cd0ceaf7d
Kavindu-J22 <it21032806@my.sliit.lk>
2023-02-23 23:22:27 +0530

    update

--- Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/LandingPage/contactFooter.js
+++ Ayanna Kiyanna Management System/New mern AyannaKiyanna/client/src/components/mainpages/LandingPage/contactFooter.js
@@ -47,8 +47,7 @@ function Contactfooter(){
                         <span id="pp"></span>
                         <span id="qq"></span>
                         <span id="rr"></span>
-                        <span id="ss"></span>
-                        <span id="tt"></span>
+                        
                         
                     </div>
 
